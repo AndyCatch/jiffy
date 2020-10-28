ba5b4227135616b265d38433a6544e64880bd75e
Andy Catch <andrew.catch@gmail.com>
2020-10-27 22:47:32 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header grid">
+      <h1 className="title">Jiffy</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const [searchTerm, useSearchTerm] = useState("")
+
+  // create-react-app lets you use arrow funcs, removing need to bind()
+  const handleChange = (event) => {
+    const { value } = event.target
+    if (value.length > 2) {
+    }
+  }
+
+  const handleKeyPress = (event) => {
+    const { value } = event.target
+
+    // when we have 2+ chars in our search box && pressed enter: run search
+    if (value.length > 2 && event.key === "Enter") {
+      alert(`search for ${value}`)
+    }
+  }
+
+  return (
+    <div className="page">
+      <Header />
+      <div className="search grid">
+        {/* our stack of gif images */}
+        <input
+          className="input grid-item"
+          placeholder="Type something"
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
