347d4f7ada5b8dd7ed27b380b339e331aabe817b
AndyCatch <andrew.catch@gmail.com>
2020-10-28 12:52:30 -0400

    Update App.js
    Updated to be a class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React, { useState } from "react"
-
 const Header = () => {
   return (
     <div className="header grid">
@@ -8,27 +6,44 @@ const Header = () => {
   )
 }
 
-function App() {
-  const [searchTerm, setSearchTerm] = useState("")
-
+class App extends Component {
+  
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      searchTerm:''
+    }
+  }
+  
+  
   // create-react-app lets you use arrow funcs, removing need to bind()
-  const handleChange = (event) => {
+  handleChange = (event) => {
     const { value } = event.target
+    this.setState((prevState, props) => ({
+      ...prevState, // take old props, spread them out
+      
+      searchTerm: value // overwrite the ones we want afterward
+    }))
     if (value.length > 2) {
+      // show hint message
     }
   }
 
-  const handleKeyPress = (event) => {
+  handleKeyPress = (event) => {
     const { value } = event.target
 
     // when we have 2+ chars in our search box && pressed enter: run search
     if (value.length > 2 && event.key === "Enter") {
-      setSearchTerm(`${value}`)
       alert(`search for ${value}`)
     }
   }
-
-  return (
+  
+  render(){
+    // can also be written 
+    // const searchTerm = this.state.searchTerm
+    const {searchTerm} = this.state
+    return (
     <div className="page">
       <Header />
       <div className="search grid">
@@ -36,13 +51,16 @@ function App() {
         <input
           className="input grid-item"
           placeholder="Type something"
-          onChange={handleChange}
-          onKeyPress={handleKeyPress}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
           value={searchTerm}
         />
       </div>
     </div>
   )
+  }
+
+
 }
 
 export default App
