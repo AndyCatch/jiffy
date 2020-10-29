137da7f3e9619232ee7270208e202be71013e04c
Andy Catch <andrew.catch@gmail.com>
2020-10-28 22:23:52 -0400

    Split off Gif component, updated loading state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Component } from "react"
+import { Component } from 'react'
+import loader from './assets/loader.svg'
+import Gif from './Gif'
+
+const randomChoice = (arr) => {
+  const randIndex = Math.floor(Math.random() * arr.length)
+  return arr[randIndex]
+}
 
 const Header = () => {
   return (
@@ -8,12 +15,53 @@ const Header = () => {
   )
 }
 
+const UserHint = ({ loading, hintText }) => (
+  <div className="user-hint">
+    {/* loader is pulled via Webpack */}
+    {/* Is the returned status loading ? (Y) = Loader : (N) = text */}
+    {loading ? (
+      <img className="block mx-auto" src={loader} alt="loader showing" />
+    ) : (
+      hintText
+    )}
+  </div>
+)
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      searchTerm: "",
+      searchTerm: '',
+      hintText: '',
+      gif: null,
+      gifs: [],
+    }
+  }
+
+  searchGiphy = async (searchTerm) => {
+    this.setState({
+      loading: true,
+    })
+
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=skOxpxRE3EQVYqxKBrfILVh9zEKTqrbQ&q=${searchTerm}&limit=25&offset=0&rating=pg&lang=en`
+      )
+      // {data} = better way of writing data.data
+      const { data } = await response.json()
+
+      const randomGif = randomChoice(data)
+
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gif: randomGif,
+        gifs: [...prevState.gifs, randomGif],
+        loading: false, // we need to do this otherwise the spinner stays onscreen
+      }))
+      // console.log(data)
+    } catch (error) {
+      //
     }
   }
 
@@ -26,30 +74,31 @@ class App extends Component {
       ...prevState, // take old props, spread them out
 
       searchTerm: value, // overwrite the ones we want afterward
+
+      // Setting a test for the length of the input
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : '',
     }))
-    if (value.length > 2) {
-      // show hint message
-    }
   }
 
   handleKeyPress = (event) => {
     const { value } = event.target
 
     // when we have 2+ chars in our search box && pressed enter: run search
-    if (value.length > 2 && event.key === "Enter") {
-      alert(`search for ${value}`)
+    if (value.length > 2 && event.key === 'Enter') {
+      this.searchGiphy(value)
     }
   }
 
   render() {
-    // can also be written
+    // could also be written
     // const searchTerm = this.state.searchTerm
-    const { searchTerm } = this.state
+    const { searchTerm, gif } = this.state
     return (
       <div className="page">
         <Header />
         <div className="search grid">
           {/* our stack of gif images */}
+          {gif && this.state.gifs.map((gif) => <Gif {...gif} />)}
           <input
             className="input grid-item"
             placeholder="Type something"
@@ -58,6 +107,7 @@ class App extends Component {
             value={searchTerm}
           />
         </div>
+        <UserHint {...this.state} />
       </div>
     )
   }
